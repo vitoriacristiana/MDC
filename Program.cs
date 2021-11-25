b986bdcea8d7a7ba6d2084b26153827c79c6e54b
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-11-25 18:28:57 -0300

    resolução exercício MDC

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MDC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Máximo Divisor Comum (método iterativo)");
+
+            Console.Write ("Digite o 1º número (a):");
+            int  a  = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o 2º número (b):");
+            int  b  =  Convert.ToInt32 (Console.ReadLine());
+
+            int mdc = 12;
+
+            Console . WriteLine ($"MDC (a, b) = {mdc}");
+        }
+    }
+}
+            
+        
+    
+
